f96d81b618469668e320d8afc941a470fed82dfe
Leila <nb.leila10@gmail.com>
2020-05-15 19:32:41 -0500

    Make sure that all the resolvers that require a user id are first authenticated

--- src/resolvers.js
+++ src/resolvers.js
@@ -11,40 +11,40 @@ module.exports = {
     me: authenticated((_, __, { user }) => {
       return user;
     }),
-    posts(_, __, { user, models }) {
+    posts: authenticated((_, __, { user, models }) => {
       return models.Post.findMany({ author: user.id });
-    },
+    }),
 
-    post(_, { id }, { user, models }) {
+    post: authenticated((_, { id }, { user, models }) => {
       return models.Post.findOne({ id, author: user.id });
-    },
+    }),
 
-    userSettings(_, __, { user, models }) {
+    userSettings: authenticated((_, __, { user, models }) => {
       return models.Settings.findOne({ user: user.id });
-    },
+    }),
     // public resolver
     feed(_, __, { models }) {
       return models.Post.findMany();
     },
   },
   Mutation: {
-    updateSettings(_, { input }, { user, models }) {
+    updateSettings: authenticated((_, { input }, { user, models }) => {
       return models.Settings.updateOne({ user: user.id }, input);
-    },
+    }),
 
-    createPost(_, { input }, { user, models }) {
+    createPost: authenticated((_, { input }, { user, models }) => {
       const post = models.Post.createOne({ ...input, author: user.id });
       pubsub.publish(NEW_POST, { newPost: post });
       return post;
-    },
+    }),
 
-    updateMe(_, { input }, { user, models }) {
+    updateMe: authenticated((_, { input }, { user, models }) => {
       return models.User.updateOne({ id: user.id }, input);
-    },
+    }),
     // admin role
-    invite(_, { input }, { user }) {
+    invite: authenticated((_, { input }, { user }) => {
       return { from: user.id, role: input.role, createdAt: Date.now(), email: input.email };
-    },
+    }),
 
     signup(_, { input }, { models, createToken }) {
       const existing = models.User.findOne({ email: input.email });
@@ -56,6 +56,7 @@ module.exports = {
       const token = createToken(user);
       return { token, user };
     },
+
     signin(_, { input }, { models, createToken }) {
       const user = models.User.findOne(input);
 
@@ -67,6 +68,7 @@ module.exports = {
       return { token, user };
     },
   },
+
   User: {
     posts(root, _, { user, models }) {
       if (root.id !== user.id) {
