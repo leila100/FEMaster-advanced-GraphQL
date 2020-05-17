ebdfb9d6a1cbe7e0bf198a83e233bf97059b7c64
Leila <nb.leila10@gmail.com>
2020-05-16 19:49:28 -0500

    Add query test for the posts query

--- tests/query.test.js
+++ tests/query.test.js
@@ -1,5 +1,5 @@
-const gql = require('graphql-tag')
-const createTestServer = require('./helper')
+const gql = require("graphql-tag");
+const createTestServer = require("./helper");
 const FEED = gql`
   {
     feed {
@@ -10,20 +10,60 @@ const FEED = gql`
       views
     }
   }
-`
+`;
 
-describe('queries', () => {
-  test('feed', async () => {
-    const {query} = createTestServer({
-      user: {id: 1},
+const POSTS = gql`
+  {
+    posts {
+      id
+      message
+      createdAt
+      likes
+      views
+    }
+  }
+`;
+
+describe("queries", () => {
+  test("feed", async () => {
+    const { query } = createTestServer({
+      user: { id: 1 },
       models: {
         Post: {
-          findMany: jest.fn(() => [{id: 1, message: 'hello', createdAt: 12345839, likes: 20, views: 300}])
-        }
-      }
-    })
+          findMany: jest.fn(() => [{ id: 1, message: "hello", createdAt: 12345839, likes: 20, views: 300 }]),
+        },
+      },
+    });
 
-    const res = await query({query: FEED})
-    expect(res).toMatchSnapshot()
-  })
-})
+    const res = await query({ query: FEED });
+    expect(res).toMatchSnapshot();
+  });
+
+  test("posts", async () => {
+    const { query } = createTestServer({
+      user: { id: "o8osACdhLw4uW4CzflBt4" },
+      models: {
+        Post: {
+          findMany: jest.fn(() => [
+            {
+              id: "E1y84ywOMrh9Lw4UgBkUz",
+              message: "Another message again",
+              createdAt: "1589652763001",
+              likes: 80,
+              views: 100,
+            },
+            {
+              id: "EYRxCSpmvLHGnbuvYIsoL",
+              message: "Another message",
+              createdAt: "1589652737837",
+              likes: 20,
+              views: 300,
+            },
+          ]),
+        },
+      },
+    });
+    const res = await query({ query: POSTS });
+    expect(res).toMatchSnapshot();
+  });
+});
